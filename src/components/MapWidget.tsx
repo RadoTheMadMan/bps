81e73098f1a2b48ff67b0f288aeb8fd7e0bf470c
RadoTheMadMan <155324791+RadoTheMadMan@users.noreply.github.com>
2026-06-23 20:02:20 +0000

    replaced CartoDB with OSM Dark Geospatial Layer

--- src/components/MapWidget.tsx
+++ src/components/MapWidget.tsx
@@ -39,10 +39,10 @@ export default function MapWidget({
   return (
     <div className="w-full h-full min-h-[350px] relative">
       <MapContainer center={userLocation} zoom={13} style={{ height: '100%', width: '100%' }}>
-        <TileLayer
-          attribution='© OpenStreetMap contributors'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        />
+      <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+      />
         <Marker position={userLocation} icon={userIcon}>
           <Popup><span className="text-zinc-900 font-bold">Your Position</span></Popup>
         </Marker>
